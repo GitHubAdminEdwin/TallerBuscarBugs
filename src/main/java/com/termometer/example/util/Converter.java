c8de3b20ad1947ff9c95bb3feeab6505d2bd164b
edwin <edwin@DESKTOP-KLECGU0>
2020-05-23 11:52:27 -0500

    corregir

--- src/main/java/com/termometer/example/util/Converter.java
+++ src/main/java/com/termometer/example/util/Converter.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Component;
 public class Converter {
 
    public double convertFarenheitCelsius (double farenheit) {
-       return (farenheit - 32) * 5/9;
+       return (farenheit - 32 * 5/9);
    }
 
    public double convertCelsiusFarenheit(double celsius) {
-       return (celsius * 9/5) + 32;
+       return (celsius * 9/5 + 32);
    }
 }
